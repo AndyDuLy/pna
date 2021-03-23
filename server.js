386b87b9b38e1cfc8197ec2116e4a29888b776e0
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-23 19:34:30 -0400

    adsadsads

--- server.js
+++ server.js
@@ -23,8 +23,14 @@ const routes = require('./routes/index');
 
 const app = express();
 
-app.use(cors({ exposedHeaders: 'x-token', origin: 'http://localhost:3000', }));
+app.use(cors({ exposedHeaders: 'x-token' }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(routes);
 app.use('/', (req, res) => {
