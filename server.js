67e69b196db245c19eac38b477c311b93a9c99db
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-23 19:40:41 -0400

    callback attached to routes for cors??

--- server.js
+++ server.js
@@ -26,13 +26,12 @@ const app = express();
 app.use(cors({ exposedHeaders: 'x-token' }));
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use(routes, (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(routes);
 app.use('/', (req, res) => {
   res.send("<div> Default endpoint </div>");
 });
