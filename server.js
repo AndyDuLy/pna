2bf3dae7449e01dfd41d3bc3b5ffa2ad904f3293
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-24 18:39:57 -0400

    switched to cors obejct

--- server.js
+++ server.js
@@ -26,16 +26,8 @@ app.use(express.json());
 
 app.options('*', cors());
 
-var whitelist = [
-  'http://localhost:3000*',
-];
-
 var corsOptions = {
-  origin: function(origin, callback){
-    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(null, originIsWhitelisted);
-  },
-
+  origin: "http://localhost:3000/*",
   credentials: true
 };
 
