42ac952c20358bb611fe44e1c3161f8c0aff4d45
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-24 01:00:23 -0400

    forgot enable cors lol

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const routes = require('./routes/index');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
