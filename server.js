b37b20f9f93a5e8d93343ef1ff64e5767fc619e4
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-24 00:55:23 -0400

    typo

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers", 
-    "Origin, X-Requested-With, Content-Type, Accept, Authoriaztion"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   
-  if (req.method === 'OPTIONS') {
+  /* if (req.method === 'OPTIONS') {
     res.header("Access-Contol-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
-  }
+  } */
 
   next();
 });
