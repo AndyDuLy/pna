ad8fab94c75762dd5471b108151ea8b18099b6e6
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-23 20:14:04 -0400

    remove use cors and add app.use res headers set allow origin

--- server.js
+++ server.js
@@ -23,15 +23,17 @@ const routes = require('./routes/index');
 
 const app = express();
 
-var corsOptions = {
-  exposedHeaders: 'x-token',
-  origin: 'http://localhost:3000',
-}
-
-app.use(cors(corsOptions));
+//app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(routes);
+
 app.use('/', (req, res) => {
   res.send("<div> Default endpoint </div>");
 });
